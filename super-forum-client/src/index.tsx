1143cc68e08a0aa9610251eea7f0fe4b5112b4f4
skylogin <skylogin@gmail.com>
2023-02-09 21:40:05 +0900

    add redux boilerplate

--- super-forum-client/src/index.tsx
+++ super-forum-client/src/index.tsx
@@ -4,6 +4,8 @@ import './index.css';
 import App from './App';
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
+import configureStore from "./store/configureStore";
+// import ErrorBoundary from "./components/ErrorBoundary";
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(
