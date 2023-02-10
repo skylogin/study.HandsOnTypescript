30072946a508787ec747632dcb1a7face1c356cd
skylogin <skylogin@gmail.com>
2023-02-10 19:34:19 +0900

    add registration form

--- super-forum-client/src/components/sidebar/SideBarMenus.tsx
+++ super-forum-client/src/components/sidebar/SideBarMenus.tsx
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import './SideBarMenus.css';
 import { useDispatch, useSelector } from "react-redux";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
@@ -12,8 +12,11 @@ import {
 import { AppState } from "../../store/AppState"
 import { UserProfileSetType } from "../../store/user/Reducer";
 
+import Registration from "../auth/Registration";
 
 const SideBarMenus = () => {
+  const [showRegister, setShowRegister] = useState(false);
+
   const user = useSelector((state: AppState) => state.user);
   const dispatch = useDispatch();
 
@@ -27,11 +30,26 @@ const SideBarMenus = () => {
     });
   }, [dispatch]);
 
+  
+  const onClickToggleRegister = () => {
+    setShowRegister(!showRegister);
+  };
+
   return (
     <>
       <ul>
-        <FontAwesomeIcon icon={faUser} />
-        <span className="menu-name">{user?.userName}</span>
+        <li>
+          <FontAwesomeIcon icon={faUser} />
+          <span className="menu-name">{user?.userName}</span>
+        </li>
+        <li>
+          <FontAwesomeIcon icon={faRegistered} />
+          <span onClick={onClickToggleRegister} className="menu-name">register</span>
+          <Registration
+            isOpen={showRegister}
+            onClickToggle={onClickToggleRegister}
+          />
+        </li>
       </ul>
     </>
   );
