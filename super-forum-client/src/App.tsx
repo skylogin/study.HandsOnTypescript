ba6d3fedb0968c7f065fdb1e10f7ef8c7188a876
skylogin <skylogin@gmail.com>
2023-02-10 21:34:18 +0900

    add rightmenu

--- super-forum-client/src/App.tsx
+++ super-forum-client/src/App.tsx
@@ -5,7 +5,7 @@ import './App.css';
 import LeftMenu from './components/areas/LeftMenu';
 import Main from './components/areas/main/Main';
 import Nav from './components/areas/Nav';
-import RightMenu from './components/areas/RightMenu';
+import RightMenu from './components/areas/rightMenu/RightMenu';
 import SideBar from './components/areas/sidebar/SideBar';
 
 import Home from './components/routes/Home';
